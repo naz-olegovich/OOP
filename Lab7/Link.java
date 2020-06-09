494d8473fd6c8067952d61b1ad943d20dd0d2255
Nazar Chopyk <61637619+naz-olegovich@users.noreply.github.com>
2020-06-09 16:14:13 +0300

    Final code

---
+++ Lab7/Link.java
@@ -0,0 +1,24 @@
+package Lab7;
+import Lab6.Track;
+
+
+public class Link {
+    private Link next;
+    private Track data;
+
+    public Link getNext() {
+        return next;
+    }
+
+    public void setNext(Link next) {
+        this.next = next;
+    }
+
+    public Track getData() {
+        return data;
+    }
+
+    public void setData(Track data) {
+        this.data = data;
+    }
+}
